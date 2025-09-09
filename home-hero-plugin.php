1c0644007b317ae26ac88b717e0c88b6ff7cfcfa
Corbin Schappel <cschappe@utk.edu>
2025-09-09 16:39:41 -0400

    chore(version): bump version

--- home-hero-plugin.php
+++ home-hero-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Home Hero
  * Plugin URI:        https://github.com/utkwdn/home-hero-plugin
  * Description:       Adds an editable Home Hero custom block
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            The University of Tennessee, Knoxville
