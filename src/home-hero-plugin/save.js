e75ead7dc8bdd242dadbce260d51dbcb7c341f88
Mark Naylor <mnaylor@utk.edu>
2026-04-24 09:31:28 -0500

    refactor(hero): add alt tags, adjust editor ui

--- src/home-hero-plugin/save.js
+++ src/home-hero-plugin/save.js
@@ -5,58 +5,69 @@ export default function save( { attributes } ) {
 
 	return (
 		<div
-			{ ...useBlockProps.save() }
-			className="home-hero-container alignfull fade-in"
+			{ ...useBlockProps.save( {
+				className: 'home-hero-container alignfull fade-in',
+			} ) }
 		>
 			<div className="home-hero">
 				<div className="home-hero-image-1">
 					<picture>
-						{ image1 && (
+						{ image1?.url && (
 							<img
 								className="fade-right"
-								src={ image1 }
-								alt="Photo 1"
+								src={ image1.url }
+								alt={ image1.alt || '' }
 							/>
 						) }
 					</picture>
 				</div>
+
 				<div className="title-and-text-shadow fade-left"></div>
+
 				<div className="title-and-text fade-left-after">
 					<div className="inner-blocks-area">
 						<InnerBlocks.Content />
 					</div>
 				</div>
+
 				<div className="home-hero-grid">
 					<div className="home-hero-image-2">
-						{ image2 && (
+						{ image2?.url && (
 							<picture>
 								<img
 									className="fade-up"
-									src={ image2 }
-									alt="Photo 2"
+									src={ image2.url }
+									alt={ image2.alt || '' }
 								/>
 							</picture>
 						) }
 					</div>
+
 					<div className="home-hero-image-3">
-						{ image3 && (
+						{ image3?.url && (
 							<picture>
 								<img
 									className="fade-up"
-									src={ image3 }
-									alt="Photo 3"
+									src={ image3.url }
+									alt={ image3.alt || '' }
 								/>
 							</picture>
 						) }
 					</div>
+
 					<div className="logo-block">
 						<div className="logo-block-image fade-in">
 							<picture>
-								<img
-									className="fade-up"
-									src={ image4 }
-									alt="Photo 4"
-								/>
+								{ image4?.url && (
+									<img
+										className="fade-up"
+										src={ image4.url }
+										alt={ image4.alt || '' }
+										aria-label={
+											image4.alt || 'Logo Graphic'
+										}
+									/>
+								) }
 							</picture>
 						</div>
 					</div>
